d67e2593713548779381aff8490aeab3a12a4fb2
putty <minprogramador@gmail.com>
2022-04-16 18:38:11 -0300

    types ok

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Second from './components/Second';
 import Destruct, {Category} from './components/Destruct';
 import State from './components/State';
 
+type textOrNull = string | null;
+type fixed = "isso" | "ou" | "aquilo";
+
 function App() {
   const name: string = "demo";
   const age: number = 30;
@@ -12,6 +15,11 @@ function App() {
   const func = (name: string): string => {
     return `ola ${name}`;
   };
+
+  const mytext:textOrNull = "tem texto aqui";
+
+  const textandofixed: fixed = "isso";
+
   return (
     <div className="App">
       <header className="App-header">
@@ -25,6 +33,8 @@ function App() {
         <Second name="manoel"/>
         <Destruct title='titulo' content="content" comentsQty={10} tags={['19', '20']} categoria={Category.TS}/>
         <State/>
+        {mytext}
+        {textandofixed}
       </header>
     </div>
   );
